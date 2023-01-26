76aa904beb7e874ed8da68cd8fa5de09ebd8f075
Priyal2807 <priyalsobti@gmail.com>
2023-01-26 16:00:31 +0530

    changing layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 import Todos from './Todo'
 import AddForm from './AddForm'
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import About from './components/About'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Post from './components/Post'
 
 class App extends Component {
     state = {
         todos: [
-            { id:1, data: "play" },
-            { id:2, data: "sleep" }
+            { id:1, data: "delete this by clicking on it and enter your first todo" }
 
         ]
     }
@@ -37,10 +33,7 @@ class App extends Component {
             <div className="container">
                 
                 <BrowserRouter>
-                    <Navbar /> 
                     <Routes>
-                        <Route exact path="/" element={<Home />} />
-                        <Route exact path="/about" element={<About />} />
                         <Route exact path="/:post_id" element={<Post/>}/>
                     </Routes>
                 </BrowserRouter>
